5486a0993475aa1229c1af13691b97765b88a753
Edward <eduard.tymoshuk@gmail.com>
2024-07-14 12:54:03 +0200

    style:added header component

---
+++ src/config/index.ts
@@ -0,0 +1,27 @@
+export const MENU_ITEMS = [
+	{
+		label: 'GŁÓWNA',
+		link: '/',
+	},
+	{
+		label: 'O NAS',
+		link: '/about'
+	},
+	{
+		label: 'OFERTA',
+		link: '/offer'
+	},
+	{
+		label: 'MENU',
+		link: '/menu'
+	},
+	{
+		label: 'GALERIA',
+		link: '/galery'
+	},
+	{
+		label: 'KONTAKT',
+		link: '/contact'
+	},
+
+]
