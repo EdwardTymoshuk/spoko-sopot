cddc602ec017285b4b98b3e2c919d0b151e1d947
Edward <eduard.tymoshuk@gmail.com>
2024-12-06 22:33:20 +0100

    feat: Update special dishes display and add Google reviews button
    - Modified the display method for special dishes to enhance clarity
    - Added a button linking to Google reviews for better user engagement

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: 'pub-6b43bbf3eb884034863e85b0eefe37a8.r2.dev', // Додайте ваш хост
-				pathname: '/**', // Дозволити всі шляхи
+				hostname: 'pub-6b43bbf3eb884034863e85b0eefe37a8.r2.dev',
+				pathname: '/**',
 			},
 		],
 	},
 }
 
-export default nextConfig;
+export default nextConfig
