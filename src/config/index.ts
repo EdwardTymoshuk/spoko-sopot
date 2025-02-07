ef093c092b7cab5729eb52734baa095f8a89893e
Edward <eduard.tymoshuk@gmail.com>
2025-02-07 07:24:14 +0100

    fix: fix allowed menu categories

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-import { CarouselImage, MongoDBReview, NavBarItem } from '@/app/types'
+import { CarouselImage, MenuItemCategory, MongoDBReview, NavBarItem } from '@/app/types'
 
 export const isOrderingOpen = false
 
@@ -77,3 +77,47 @@ export const OPINIONS: MongoDBReview[] = [
 		rating: 5,
 	},
 ]
+
+// Масив дозволених категорій
+export const allowedCategories: MenuItemCategory[] = [
+	'Dania główne',
+	'Burgery',
+	'Makarony/Ravioli',
+	'Dla dzieci',
+	'Dodatki',
+	'Desery',
+	'Napoje',
+	'Śniadania',
+	'Przystawki',
+	'Zupy',
+	'Bowle',
+	'Vege',
+	'Dania rybne',
+	'Owoce morza',
+	'Dania mięsne',
+	'Klasyczne koktaile',
+	'Na ciepło',
+	'Herbata',
+	'Kawa',
+	'Napoje zimne',
+	'Piwo butelkowe',
+	'Piwo bezalkoholowe',
+	'Piwo beczkowe',
+	'Piwo smakowe',
+	'Wina Białe',
+	'Wina Czerwone',
+	'Wina Musujące',
+	'Wina Różowe',
+	'Drinki',
+	'Whisky',
+	'Rum',
+	'Gin',
+	'Tequila',
+	'Cognac / Brandy',
+	'Wódka',
+	'Napoje alkoholowe',
+	'Napoje bezalkoholowe',
+	'Napoje łekkoprocentowe',
+	'Inne'
+  ];
+
