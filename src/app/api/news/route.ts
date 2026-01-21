b5f1bb109c477fc5e0f2f95fc3c0bb3b6183d08c
Edward <eduard.tymoshuk@gmail.com>
2026-01-21 19:34:00 +0100

    fix: fix news page

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb'
 import { NextResponse } from 'next/server'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   try {
     const client = await MongoClient.connect(process.env.MONGODB_URI!)
@@ -11,18 +13,22 @@ export async function GET() {
     client.close()
 
     const formattedNews = news.map((item) => ({
-      id: item._id.toString(), // Konwersja ObjectId do stringa
+      id: item._id.toString(),
       title: item.title,
       image: item.image || '',
       description: item.description,
-      fullDescription: item.fullDescription,
-      galleryImages: item.galleryImages.map((img: string) => ({
+      fullDescription: item.fullDescription || '',
+      galleryImages: (item.galleryImages || []).map((img: string) => ({
         src: img,
         thumbnail: img,
       })),
     }))
 
-    return NextResponse.json(formattedNews)
+    return NextResponse.json(formattedNews, {
+      headers: {
+        'Cache-Control': 'no-store',
+      },
+    })
   } catch (error) {
     console.error('Błąd pobierania aktualności:', error)
     return NextResponse.json(
